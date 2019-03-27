66b3454385ea24cf4d55c74f2b9556c3b25bc1a5
SamatBagdan <43234468+SamatBagdan@users.noreply.github.com>
2019-03-28 03:53:02 +0600

    Threads

---
+++ repos/pp2/lab6-pp2/task1/ConsoleApp1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Thread1
+{                                      //Решение может быть неправильным
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int n = 3;
+           Thread[] threads1 = new Thread[n];
+            Thread thread1 = new Thread(DoIt);
+            Thread thread2 = new Thread(DoIt1);
+            Thread thread3 = new Thread(DoIt2);
+            for(int i=0; i<n; i++)
+            {
+                thread1.Start();
+                thread2.Start();
+                thread3.Start();
+            }
+
+
+        }
+
+         private static void DoIt()
+        {
+            Thread.CurrentThread.Name = "Samat";
+            for (int i = 0; i < 3; i++)
+            {
+                
+                Console.WriteLine(Thread.CurrentThread.Name);
+            }
+        }
+
+        private static void DoIt1()
+        {
+            Thread.CurrentThread.Name = "Bagdan";
+           for (int i = 0; i < 3; i++)
+           {
+
+                Console.WriteLine(Thread.CurrentThread.Name);
+           }
+
+        }
+        private static void DoIt2()
+        {
+            Thread.CurrentThread.Name = "Mirbulatuly";
+           for (int i = 0; i < 3; i++)
+           {
+
+                Console.WriteLine(Thread.CurrentThread.Name);
+           }
+
+        }
+    }
+}
---
+++ repos/pp2/lab6-pp2/task2/task2/task2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Thread thread1 = new Thread(mythread1);
+            Thread thread2 = new Thread(mythread2);
+            Thread thread3 = new Thread(mythread3);
+            /*  thread1.Priority = ThreadPriority.Highest;                   //   Highest - самый высокий
+              thread2.Priority = ThreadPriority.AboveNormal;                 //   AboveNormal - выше среднего
+              thread2.Priority = ThreadPriority.AboveNormal;                 //   Normal - стандартный
+              thread3.Priority = ThreadPriority.Lowest;                      //   BelowNormal - ниже среднего
+                                                                             //   Lowest - самый низкий */
+            
+
+
+
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+        }
+        static void mythread2()
+        {
+            for (int i = 1; i <= 4; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("Поток 2 выводит " + i);
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Поток 2 = Завершился");
+        }
+        static void mythread1()
+        {
+            for (int i = 1; i <= 4; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("Поток 1 выводит " + i);
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Поток 1 = Завершился");
+        }
+
+         static void mythread3()
+         {
+                for (int i = 1; i <= 4; i++)
+                {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Поток 3 выводит " + i);
+                }
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Поток 3 = Завершился");
+         }
+     }
+}
