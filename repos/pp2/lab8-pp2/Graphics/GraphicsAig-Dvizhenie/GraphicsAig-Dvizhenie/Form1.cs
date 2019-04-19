6a42f41975c2be1b86dffce86bbb634c8740b50b
SBM <43234468+SBMKBTU@users.noreply.github.com>
2019-04-20 00:26:13 +0600

    Add files via upload

---
+++ repos/pp2/lab8-pp2/Graphics/GraphicsAig-Dvizhenie/GraphicsAig-Dvizhenie/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraphicsAig_Dvizhenie
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Star s1 = new Star(120, 200);
+        Star s2 = new Star(400, 300);
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Width = 800;
+            this.Height = 600;
+            
+        }
+       
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            this.BackColor = Color.Black;
+            SolidBrush blue = new SolidBrush(Color.Blue);
+            e.Graphics.FillRectangle(blue, 10, 10, 760, 540);
+
+            SolidBrush white = new SolidBrush(Color.White);
+            e.Graphics.FillEllipse(white, 50, 100, 20, 20);
+            e.Graphics.FillEllipse(white, 200, 50, 20, 20);
+            e.Graphics.FillEllipse(white, 350, 100, 20, 20);
+
+
+            int y = 200, x = 300;
+            Point[] spaceship =
+            {
+                new Point(x,y),
+                new Point(x+40,y+20),
+                new Point(x+40,y+50),
+                new Point(x,y+70),
+                new Point(x-40,y+50),
+                 new Point(x-40,y+20)
+            };
+
+            SolidBrush yellow = new SolidBrush(Color.Yellow);
+            e.Graphics.FillPolygon(yellow, spaceship);
+
+            SolidBrush red = new SolidBrush(Color.Red);
+
+            e.Graphics.FillPath(red, s1.gp1);
+            e.Graphics.FillPath(red, s1.gp2);
+            e.Graphics.FillPath(red, s2.gp1);
+            e.Graphics.FillPath(red, s2.gp2);
+
+            x = 360;
+            y = 180;
+            
+            Point[] newstar =
+            {
+                   new Point(x,y),
+                   new Point(x+5,y+25),
+                   new Point(x+20,y+30),
+                    new Point(x+5,y+35),
+                    new Point(x,y+60),
+
+                    new Point(x-5,y+35),
+                  new Point(x-20,y+30),
+                  new Point(x-5,y+25),
+
+            };
+            e.Graphics.FillClosedCurve(yellow, newstar);
+
+           
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            s1.Move();
+           s2.Move();
+            Refresh();
+        }
+    }
+}
---
+++ repos/pp2/lab8-pp2/Graphics/GraphicsAig-Dvizhenie/GraphicsAig-Dvizhenie/Star.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphicsAig_Dvizhenie
+{
+    class Star
+    {
+        int x, y;
+        public GraphicsPath gp1;
+        public GraphicsPath gp2;
+      
+
+        public Star(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+            gp1 = new GraphicsPath();
+            gp2 = new GraphicsPath();
+            ////////////////////
+           
+            
+
+
+
+            if (x == 780) x = 0;
+            gp1.Reset();
+            gp2.Reset();
+            Point[] firstT =
+           {
+                new Point (x,y),
+                new Point (x+20,y+30),
+                new Point(x-20,y+30)
+            };
+            gp1.AddPolygon(firstT);
+            Point[] secondT =
+          {
+                new Point (x-20,y+10),
+                new Point (x+20,y+10),
+                new Point(x,y+40)
+            };
+            gp2.AddPolygon(secondT);
+            //////////////////////
+
+        }
+
+       
+
+
+
+        public void Move()
+        {
+                x += 10;
+              if (x == 780) x = 0;
+           
+
+            gp1.Reset();
+            gp2.Reset();
+            Point[] firstT =
+           {
+                new Point (x,y),
+                new Point (x+20,y+30),
+                new Point(x-20,y+30)
+            };
+            gp1.AddPolygon(firstT);
+            Point[] secondT =
+          {
+                new Point (x-20,y+10),
+                new Point (x+20,y+10),
+                new Point(x,y+40)
+            };
+            gp2.AddPolygon(secondT);
+           
+        }
+    }
+}
+    
+
