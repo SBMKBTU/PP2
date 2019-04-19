e7c16e0105f0e729221e83d61eec04a999f1a76c
SBM <43234468+SBMKBTU@users.noreply.github.com>
2019-04-20 00:22:40 +0600

    Add files via upload

---
+++ repos/pp2/lab8-pp2/Graphics/DvizheniePoPorabole/DvizheniePoPorabole/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DvizheniePoPorabole
+{
+    public partial class Form1 : Form
+    {
+        Func<float, float> function = (x) => -x * x;
+        private float time = 0f;
+        private TrackBar tb;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.OptimizedDoubleBuffer | ControlStyles.UserPaint, true);
+
+            tb = new TrackBar { Minimum = 5, Maximum = 20, Parent = this };
+            Application.Idle += (o, e) => Invalidate();
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            var dt = tb.Value / 4000f;
+            var p = VirtualToAbsolute(time, function(time));
+            e.Graphics.FillEllipse(Brushes.Green, p.X - 5, p.Y - 5, 10, 10);
+
+            var points = new List<PointF>();
+            for (var t = 0f; t <= 1; t += dt)
+                points.Add(VirtualToAbsolute(t, function(t)));
+
+            e.Graphics.DrawLines(Pens.Maroon, points.ToArray());
+
+            if (p.X > Height)
+                time = 0;
+
+            time += dt;
+        }
+        PointF VirtualToAbsolute(float x, float y)
+        {
+            return new PointF(x * Width, Height / 2 - y * Height);
+        }
+
+    }
+}
---
+++ repos/pp2/lab8-pp2/Graphics/DvizheniePoSpirali/DvizheniePoSpirali/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DvizheniePoSpirali
+{
+    public partial class Form1 : Form
+    {
+        private Point pole;//центр спирали
+        private Point center;//центр окружности
+        private Timer timer = new Timer();//таймер
+        private Pen pen = new Pen(Color.Red, 2);//Перо для рисования окружности
+        private Pen curvepen = new Pen(Color.Blue, 1) { DashStyle = System.Drawing.Drawing2D.DashStyle.Dash };
+        private float diameter = 30f;//Диаметр окружности
+        private float a = 10;//Шаг спирали
+        private float phi;//угол поворота спирали
+        private List<Point> pts;
+        private Point[] ptar;
+
+        public Form1()
+        {
+            InitializeComponent();
+            pole = new Point(this.ClientRectangle.Width / 2, this.ClientRectangle.Height / 2);
+            pts = new List<Point>();
+            timer.Tick += new EventHandler(timer_tick);
+            this.Paint += new PaintEventHandler(Form1_Paint);
+            timer.Interval = 100;
+            timer.Enabled = true;
+        }
+        
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            try
+            {
+                if (timer.Enabled)
+                    using (Graphics g = e.Graphics)
+                    {
+                        //Перенос начала координат в центр клиентской части формы
+                        g.TranslateTransform(pole.X, pole.Y);
+                        //рисование окружности в заданных координатах
+                        g.DrawEllipse(pen, center.X - diameter / 2, center.Y - diameter / 2, diameter, diameter);
+                        //Прорисовка спирали
+                        g.DrawCurve(curvepen, ptar);
+                        /*граничные условия. Если центр окружности выходит за клиентскую 
+                        область формы, то начинаем сначала*/
+                        center.Offset(pole);
+                        if (!this.ClientRectangle.Contains(center)) Reset();
+                    }
+            }
+            catch (Exception) { }
+        }
+
+        private void timer_tick(object sender, EventArgs e)
+        {
+            center = new Point();
+            center.X = (int)((a / (Math.PI * 2)) * phi * Math.Cos(phi));
+            center.Y = (int)((a / (Math.PI * 2)) * phi * Math.Sin(phi));
+            pts.Add(center);
+            ptar = new Point[pts.Count];
+            pts.CopyTo(ptar);
+            this.Refresh();
+            phi += 0.8f;
+        }
+
+        private void Reset()
+        {
+            phi = 0;
+            pts.Clear();
+            ptar = null;
+        }
+    }
+}
